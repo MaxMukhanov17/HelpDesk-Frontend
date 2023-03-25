9da21c3f82ec281d299e81c111dbcf3c3af28456
Maxim <momcmop@icloud.com>
2023-03-25 19:23:57 +0300

    First commit

---
+++ src/js/API/ajaxSend.js
@@ -0,0 +1,20 @@
+const ajaxSend = async (options) => {
+  const URL = "https://alexa1920.herokuapp.com/";
+  const requestUrl = `${URL}?${options.query}`;
+
+  const request = await fetch(requestUrl, {
+    method: options.method,
+    body: options.data ? JSON.stringify(options.data) : null,
+  });
+
+  console.log(request, "request");
+  const response = await request.json();
+  console.log(response, "response");
+  if (options.callback) {
+    options.callback(response);
+  }
+
+  return response;
+};
+
+export default ajaxSend;
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import TicketsList from "./TicketsList";
+import Controller from "./Controller";
+
+const container = document.getElementById("container");
+const ticketsList = new TicketsList(container);
+
+const controller = new Controller(ticketsList);
+controller.init();
